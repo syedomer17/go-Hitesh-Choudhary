9c1c873e53f5102f7830cb8b467569080afe8bd4
syedomer17 <syedomerali2006@gmail.com>
2026-02-22 22:06:24 +0530

    Updated by Omer on 2026-02-22 at 10:06PM

--- 1_GOLANG_BASICS/17_map/main.go
+++ 1_GOLANG_BASICS/17_map/main.go
@@ -1,5 +1,34 @@
-package main 
+package main
 
-func main(){
-	
+import "fmt"
+
+func main() {
+	// map[keyType]valueType
+	ages := map[string]int{
+		"Omer": 19,
+		"Ali":  20,
+	}
+
+	fmt.Println(ages["Omer"], len(ages))
+
+	var scores map[string]int // this is an nil map 
+
+	fmt.Println(scores)
+
+	scores = make(map[string]int)
+
+	scores["Math"] = 90 
+
+	fmt.Println(scores)
+
+	users := map[string]string{
+		"user1": "Omer",
+		"user2": "Ali",
+	}
+	fmt.Println(users)
+
+	// to delete an key value pair from a map we can use the delete function
+
+	delete(users, "user2")
+	fmt.Println(users)
 }
