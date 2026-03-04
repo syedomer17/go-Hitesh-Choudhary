7ff26503babbf7f9311b1f5efa8e92504e9e936b
syedomer17 <syedomerali2006@gmail.com>
2026-03-04 16:47:31 +0530

    Updated by Omer on 2026-03-04 at 04:47PM

--- 5_GOLANG_AUTH/internal/httpserver/router.go
+++ 5_GOLANG_AUTH/internal/httpserver/router.go
@@ -2,7 +2,9 @@ package httpserver
 
 import (
 	"go-auth/internal/app"
+	"go-auth/internal/middleware"
 	"go-auth/internal/user"
+	"net/http"
 
 	"github.com/gin-gonic/gin"
 )
@@ -23,8 +25,45 @@ func NewRouter(a *app.App) *gin.Engine {
 	userHandler := user.NewHandler(userSvc)
 
 	router.POST("/register", userHandler.Register)
-
 	router.POST("/login",userHandler.Login)
+
+	api := router.Group("/api")
+
+	api.Use(middleware.AuthRequired(a.Config.JWTSecret))
+
+	api.GET("/files", func(c *gin.Context){
+		userId, _ := middleware.GetUserID(c)
+		role, _ := middleware.GetRole(c)
+
+		c.JSON(http.StatusOK, gin.H{
+			"ok": true,
+			"userId": userId,
+			"role": role,
+			"files": []any{},
+		})
+		return
+	})
+
+	api.GET("/products", func(c *gin.Context){
+		c.JSON(http.StatusOK, gin.H{
+			"ok": true,
+			"products": []any{},
+		})
+		return
+	})
+
+	admin := api.Group("/admin")
+
+	admin.Use(middleware.RequiredAdmin())
+
+	admin.GET("/getme",func(c *gin.Context){
+		role, _ := middleware.GetRole(c)
+		c.JSON(http.StatusOK, gin.H{
+			"ok": true,
+			"role": role,
+		})
+		return
+	})
 
 	return router
 }
