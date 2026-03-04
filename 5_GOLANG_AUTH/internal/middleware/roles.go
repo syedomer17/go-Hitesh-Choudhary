2557319dfd9cfa57ab638c53b3bbd8d20823858b
syedomer17 <syedomerali2006@gmail.com>
2026-03-04 16:25:21 +0530

    Updated by Omer on 2026-03-04 at 04:25PM

--- 5_GOLANG_AUTH/internal/middleware/roles.go
+++ 5_GOLANG_AUTH/internal/middleware/roles.go
@@ -1,7 +1,28 @@
 package middleware
 
-import "github.com/gin-gonic/gin"
+import (
+	"net/http"
+	"strings"
+
+	"github.com/gin-gonic/gin"
+)
 
 func RequiredAdmin() gin.HandlerFunc {
-	
+	return func (c *gin.Context) {
+		role, ok := GetRole(c)
+
+		if !ok {
+			c.AbortWithStatusJSON(http.StatusUnauthorized,gin.H{
+				"error":"Unauthorized",
+			})
+			return
+		}
+		if !strings.EqualFold(role,"admin"){
+			c.AbortWithStatusJSON(http.StatusForbidden,gin.H{
+				"error":"Insufficient access",
+			})
+			return
+		} 
+		c.Next()
+	}
 }
