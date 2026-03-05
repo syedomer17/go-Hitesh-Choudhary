60ee946925f15c908350979ccb5437afcedac576
syedomer17 <syedomerali2006@gmail.com>
2026-03-05 19:48:58 +0530

    Updated by Omer on 2026-03-05 at 07:48PM

---
+++ 6_GOLANG_CONCURRENCY/01_groutine/main.go
@@ -0,0 +1,14 @@
+package main 
+
+func main() {
+	//doing multiple tasks in a way that thay can overlap in time, instead of doing them one by one 
+
+	/*
+	concurrency: doing multiple tasks at same time but not necessarily at the same time
+	parallelism: doing multiple tasks at the same time
+	*/
+
+	// golang code can be concurrect without being parallel, but it can be parallel without being concurrent
+
+	
+}
